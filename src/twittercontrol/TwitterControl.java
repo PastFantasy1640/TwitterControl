8792a010f729468f9f4153e07fb706ca89026325
PastFantasy1640 <pastfantasy1640@gmail.com>
2016-08-19 14:48:56 +0900

    make_projects

---
+++ src/twittercontrol/TwitterControl.java
@@ -0,0 +1,19 @@
+/*
+ * (c) 2016 white TwitterControl.java
+ */
+package twittercontrol;
+
+/**
+ *
+ * @author white
+ */
+public class TwitterControl {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
